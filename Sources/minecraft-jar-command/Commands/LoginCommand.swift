102860767c10521a80f257211beae153f08c69ca
Ezekiel Elin <ezekielelin@me.com>
2021-01-17 20:36:38 -0500

    Save/read access tokens to UserDefaults

--- Sources/minecraft-jar-command/Commands/LoginCommand.swift
+++ Sources/minecraft-jar-command/Commands/LoginCommand.swift
@@ -15,17 +15,33 @@ struct LoginCommand: ParsableCommand {
         abstract: "Login and fetch an authentication token"
     )
     
-    @Argument
+    @Argument(help: "Mojang Email Address")
     var username: String
     
-    @Argument
+    @Argument(help: "Mojang Password")
     var password: String
     
-    @Option(name: [.long, .customShort("c")], help: "hi")
+    @Option(name: [.long, .customShort("c")], help: "Client Token")
     var clientToken: String?
 
+    @Flag(name: [.long, .customShort("s")])
+    var saveCredentials: Bool = false
+    
     func run() throws {
-        let auth = try AuthenticationManager.authenticate(username: username, password: password, clientToken: clientToken)
+        let defaults = UserDefaults.standard
+        
+        let savedClientToken = defaults.string(forKey: "clientToken")
+        if savedClientToken != nil && clientToken == nil {
+            print("Client Token Loaded...")
+        }
+        
+        let auth = try AuthenticationManager.authenticate(username: username, password: password, clientToken: clientToken ?? savedClientToken)
+        
+        if saveCredentials {
+            defaults.set(auth.clientToken, forKey: "clientToken")
+            defaults.set(auth.accessToken, forKey: "accessToken")
+            print("Credentials Saved...")
+        }
         
         print("Authentication Successful")
         print("=========================")
