1b32bc37cf4786d45576988ba77bf2c1a6063199
Ezekiel Elin <ezekielelin@me.com>
2021-10-25 21:30:01 -0400

    Fixed launcher profiles access levels

--- Sources/InstallationManager/Auth/LauncherProfiles.swift
+++ Sources/InstallationManager/Auth/LauncherProfiles.swift
@@ -19,19 +19,42 @@ public struct LauncherProfiles: Decodable {
     var selectedProfile: AuthenticationDatabaseEntry.AuthProfile? {
         return self.selectedAccount?.profiles[self.selectedUser.profile]
     }
+
+    public init(authenticationDatabase: [String : LauncherProfiles.AuthenticationDatabaseEntry],
+                  clientToken: String,
+                  selectedUser: LauncherProfiles.SelectedUser) {
+        self.authenticationDatabase = authenticationDatabase
+        self.clientToken = clientToken
+        self.selectedUser = selectedUser
+    }
     
-    struct AuthenticationDatabaseEntry: Decodable {
+    public struct AuthenticationDatabaseEntry: Decodable {
         let accessToken: String
         let profiles: [String: AuthProfile]
         let username: String
         
+        public init(accessToken: String, profiles: [String : String], username: String) {
+            self.accessToken = accessToken
+            self.profiles = profiles.mapValues { AuthProfile(displayName: $0) }
+            self.username = username
+        }
+        
         struct AuthProfile: Decodable {
             let displayName: String
+            
+            init(displayName: String) {
+                self.displayName = displayName
+            }
         }
     }
     
-    struct SelectedUser: Decodable {
+    public struct SelectedUser: Decodable {
         let account: String
         let profile: String
+        
+        public init(account: String, profile: String) {
+            self.account = account
+            self.profile = profile
+        }
     }
 }
